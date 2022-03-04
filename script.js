4ad36b5f2b07eebedec0b634978337088cd9f6a7
victor efidi <vefidi135@gmail.com>
2022-03-04 10:27:54 +0100

    upaded the branch with comment

--- script.js
+++ script.js
@@ -2,10 +2,13 @@ const green=document.querySelector("#green");
 const red=document.getElementById("red");
 const umber=document.querySelector("#umber");
 
+//about to set interv al....
 setInterval(light_red()
 
 , 1000)
 
+
+//i'm abou to add to my clock... 
 
 function light_red(){
     red.style.color="rgba(255, 0, 0, 0.295);"
